e63e86fba7ff7bf750b178506fd9a45a72da3958
diegoromarioH <diegoromario.work@gmail.com>
2026-09-06 03:03:59 -0600

    Cargar estilos globales de búsqueda

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/global.css";
+import "./styles/SearchBars.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
