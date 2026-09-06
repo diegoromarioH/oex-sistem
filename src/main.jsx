4a7ae82402b34e44771aa42c8dbc21d8cbcef832
diegoromarioH <diegoromario.work@gmail.com>
2026-09-06 03:11:31 -0600

    Carga mejoras responsive globales

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/global.css";
 import "./styles/SearchBars.css";
+import "./styles/ResponsiveSystem.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
